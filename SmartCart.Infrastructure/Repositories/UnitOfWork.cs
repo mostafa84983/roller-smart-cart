1a73bf0e73d8a6de1ae4925782727d472d8ff17e
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-04-24 07:18:14 +0200

    Implement Unit Of Work Patten for managing repositories

---
+++ SmartCart.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCart.Domain.Interfaces;
+using SmartCart.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public UnitOfWork(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
