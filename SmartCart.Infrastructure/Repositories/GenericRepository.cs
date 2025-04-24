38f73402e61f293460a2caea0f3cf77402d4a128
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-04-24 06:43:05 +0200

    Create Generic Repository and Its Interface

---
+++ SmartCart.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCart.Domain.Interfaces;
+using SmartCart.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Infrastructure.Repositories
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DataContext _context;
+
+        protected GenericRepository(DataContext Context)
+        {
+            _context = Context;
+        }
+        public async Task Add(T entity)
+        {
+           await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAll()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
