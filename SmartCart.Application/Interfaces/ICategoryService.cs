d024a50029df8044e43b986e0c4a75124ae9ca90
Menna Mabrouk <mmaly569@gmail.com>
2025-06-19 06:17:25 +0200

    Implement Category service

--- SmartCart.Application/Interfaces/ICategoryService.cs
+++ SmartCart.Application/Interfaces/ICategoryService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SmartCart.Application.Common;
+using SmartCart.Application.Dto;
+using SmartCart.Application.Dto.Category;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,9 @@ namespace SmartCart.Application.Interfaces
 {
     public interface ICategoryService
     {
+        Task<GenericResult<PaginatedResult<CategoryDto>>> GetAllPaginatedCategories(int page, int pageSize);
+        Task<GenericResult<IEnumerable<CategoryDto>>> GetCategoriesWithOffers();
+        Task<Result> CreateCategory(CreateCategoryDto createCategoryDto);
+        Task<Result> UpdateCategory(CategoryDto categoryDto);
     }
 }
