9407ca8033f811cde4c760a148cc1d2de5c19ba3
Menna Mabrouk <mmaly569@gmail.com>
2025-06-19 04:08:24 +0200

    Add structure of category service and its interface

--- SmartCart.Application/Interfaces/ICategoryService.cs
+++ SmartCart.Application/Interfaces/ICategoryService.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartCart.Application.Services
+namespace SmartCart.Application.Interfaces
 {
-    public class test
+    public interface ICategoryService
     {
-        //test
     }
 }
---
+++ SmartCart.Application/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SmartCart.Application.Interfaces;
+using SmartCart.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public CategoryService(IUnitOfWork unitOfWork, IMapper mapper) 
+        { 
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
