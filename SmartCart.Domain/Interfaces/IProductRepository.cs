81f3a16df4bc622a498e139f01c1f8738d89ea8d
Menna Mabrouk <mmaly569@gmail.com>
2025-06-21 07:48:59 +0200

    Implement ProductRepository with all paginated get methods

---
+++ SmartCart.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using SmartCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Domain.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<(IEnumerable<Product> Data, int TotalCount)> GetPaginatedProductsInCategory(int categoryId,int page, int pageSize);
+        Task<(IEnumerable<Product> Data, int TotalCount)> GetPaginatedProductsWithOfferInCategory(int categoryId, int page, int pageSize);
+        Task<(IEnumerable<Product> Data, int TotalCount)> GetPaginatedProductsOfOrder(int orderId, int page, int pageSize);
+        Task<Product> GetProductByCode(int productCode);
+
+    }
+}
