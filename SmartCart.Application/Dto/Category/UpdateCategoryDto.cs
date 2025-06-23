3b32c179dffed08f1944895c859fe075031e8b36
Menna Mabrouk <mmaly569@gmail.com>
2025-06-23 14:06:19 +0200

    Implement updatecategoryDTO

---
+++ SmartCart.Application/Dto/Category/UpdateCategoryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Application.Dto.Category
+{
+    public class UpdateCategoryDto
+    {
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category image is required")]
+        public string CategoryImage { get; set; }
+
+        [Required(ErrorMessage = "Category name is required")]
+        public string CategoryName { get; set; }
+    }
+}
