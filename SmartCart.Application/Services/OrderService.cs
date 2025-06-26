3ff5d2cad787d466e20b91689064710f9228d6ae
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-06-26 12:04:22 +0300

    Implement create order service

--- SmartCart.Application/Services/OrderService.cs
+++ SmartCart.Application/Services/OrderService.cs
@@ -14,17 +14,40 @@ namespace SmartCart.Application.Services
 {
     public class OrderService : IOrderService
     {
-        private readonly IUnitOfWork _unitOfWork1;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         public OrderService(IUnitOfWork unitOfWork , IMapper mapper)
         {
-            _unitOfWork1= unitOfWork;
+            _unitOfWork= unitOfWork;
             _mapper = mapper;
+        }
+
+        public async Task<Result> createOrder(int userId)
+        {
+            var user = await _unitOfWork.User.GetById(userId);
+            if (user == null)
+                return Result.Failure("User not found");
+
+            var order = new Order
+            {
+                UserId = userId,
+                OrderPrice = 0,
+                OrderDiscount = 0 ,
+                OrderNumber = $"ORD-{Guid.NewGuid().ToString().Substring(0, 8)}",
+                CreationDate = DateTime.UtcNow,
+            };
+
+             await _unitOfWork.Order.Add(order);
+            var result = _unitOfWork.Save() > 0;
+            if (result)
+                return Result.Success();
+            else
+                return Result.Failure("New order creation failed");
         }
 
         public async Task<GenericResult<IEnumerable<OrderDto>>> GetOrdersOfUser(int userId)
         {
-            var orders = await _unitOfWork1.Order.GetOrdersOfUser(userId);
+            var orders = await _unitOfWork.Order.GetOrdersOfUser(userId);
             if (orders == null)
                 return GenericResult<IEnumerable<OrderDto>>.Failure("No Order is Found");
 
