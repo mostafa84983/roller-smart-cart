9b6f1ca926786669ebe1d3815ed0365a8962a300
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-06-26 12:51:16 +0300

    Create productRequest Dto

---
+++ SmartCart.Application/Dto/Product/ProductRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCart.Application.Dto.Product
+{
+    public class ProductRequest
+    {
+        public int ProductCode { get; set; }
+        public string CartId { get; set; }
+    }
+}
