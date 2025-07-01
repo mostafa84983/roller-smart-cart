e78668ee0498741b91c8cc8aa4aa64a346db1013
Menna Mabrouk <mmaly569@gmail.com>
2025-07-01 21:33:04 +0200

    implement product models

---
+++ src/app/features/product/models/create-product.model.ts
@@ -0,0 +1,10 @@
+export interface CreateProductModel{
+productName  : string ;
+productCode : number ;
+productWeight : number ; 
+quantity : number;
+productPrice  : number ;
+productImage : string ;
+productDescription : string ;
+categoryId : number ;
+}
