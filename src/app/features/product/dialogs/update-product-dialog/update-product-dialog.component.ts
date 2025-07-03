940fab3f2c41670e3b7e254ea4ed880315242a20
Menna Mabrouk <mmaly569@gmail.com>
2025-07-03 07:25:28 +0200

    add update to product and category

---
+++ src/app/features/product/dialogs/update-product-dialog/update-product-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-product-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-product-dialog.component.html',
+  styleUrl: './update-product-dialog.component.scss'
+})
+export class UpdateProductDialogComponent {
+
+}
