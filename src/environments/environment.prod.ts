68be4b18af34219ea5c1ca46fac483ff4c0a9957
Heba Elsayed <heba.elsayed.m.116@gmail.com>
2025-07-05 17:46:39 +0300

    Update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://smartcart-api:8080/api',
-  backendBaseUrl: 'https://smartcart-api:8080',
-  signalRUrl: 'https://smartcart-api:8080/hubs' 
+  production: true, 
+  apiUrl: '/api', 
+  backendBaseUrl: '', 
+  signalRUrl: '/hubs'
 };
 
